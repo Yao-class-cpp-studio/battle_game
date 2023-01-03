55852ba512af488785036093e119fa128870a6d2
LazyJazz <zijian.lv@hotmail.com>
2023-01-03 17:35:58 +0800

    Fix error

--- src/battle_game/core/bullets/cannon_ball.cpp
+++ src/battle_game/core/bullets/cannon_ball.cpp
@@ -46,6 +46,10 @@ void CannonBall::Update() {
   }
 }
 CannonBall::~CannonBall() {
-  for (int i = 0; i < 5; i++) {game_core_->PushEventGenerateParticle<particle::Smoke>(position_, rotation_, game_core_->RandomInCircle() * 2.0f, 0.2f,glm::vec4{0.0f, 0.0f, 0.0f, 1.0f}, 3.0f);}
+  for (int i = 0; i < 5; i++) {
+    game_core_->PushEventGenerateParticle<particle::Smoke>(
+        position_, rotation_, game_core_->RandomInCircle() * 2.0f, 0.2f,
+        glm::vec4{0.0f, 0.0f, 0.0f, 1.0f}, 3.0f);
+  }
 }
 }  // namespace battle_game::bullet
