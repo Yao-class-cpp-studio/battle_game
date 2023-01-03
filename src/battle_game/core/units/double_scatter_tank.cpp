fd6a182deb399c9c8f581e60f85bcc6038555dd1
Hao Wang <58178768+wanghao-create@users.noreply.github.com>
2023-01-03 23:38:17 +0800

    delete namespace in childclass

--- src/battle_game/core/units/double_scatter_tank.cpp
+++ src/battle_game/core/units/double_scatter_tank.cpp
@@ -6,11 +6,6 @@
 
 namespace battle_game::unit {
 
-namespace {
-uint32_t tank_body_model_index = 0xffffffffu;
-uint32_t tank_turret_model_index = 0xffffffffu;
-}  // namespace
-
 DoubleScatterTank::DoubleScatterTank(GameCore *game_core,
                                      uint32_t id,
                                      uint32_t player_id)
