31b56e93642efdd69541e2b346e3224940506225
XIAOYU XIONG <107735955+Megumi-X@users.noreply.github.com>
2023-01-04 08:57:03 +0800

    Update selectable_units.cpp

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -23,6 +23,7 @@ void GameCore::GeneratePrimaryUnitList() {
    * */
   ADD_SELECTABLE_UNIT(unit::Tank);
   ADD_SELECTABLE_UNIT(unit::TankXxy);
+  ADD_SELECTABLE_UNIT(unit::DoubleScatterTank);
 
   unit.reset();
 }
