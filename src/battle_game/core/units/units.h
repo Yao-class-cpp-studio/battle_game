d1750f9d5d2bd14851829dfc9b4e539b636ee509
the-bucket-rider <61498107+the-bucket-rider@users.noreply.github.com>
2023-01-05 22:34:18 +0800

    Clang format

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -1,5 +1,5 @@
 #pragma once
 #include "battle_game/core/units/double_scatter_tank.h"
 #include "battle_game/core/units/inferno_tank.h"
-#include "battle_game/core/units/triple_shot_star_tank.h"
 #include "battle_game/core/units/tiny_tank.h"
+#include "battle_game/core/units/triple_shot_star_tank.h"
