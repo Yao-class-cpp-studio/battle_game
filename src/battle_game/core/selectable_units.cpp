762a59dadf8d86618bd0d0b6540c04d205da62e6
EDGE-qy <113410981+EDGE-qy@users.noreply.github.com>
2023-01-05 21:40:35 +0800

    [New Unit] Three Body Man (#49)

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -23,6 +23,7 @@ void GameCore::GeneratePrimaryUnitList() {
    * */
   ADD_SELECTABLE_UNIT(unit::Tank);
   ADD_SELECTABLE_UNIT(unit::DoubleScatterTank);
+  ADD_SELECTABLE_UNIT(unit::ThreeBodyMan);
   ADD_SELECTABLE_UNIT(unit::InfernoTank);
 
   unit.reset();
