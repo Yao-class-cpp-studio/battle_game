446d1fc763d5af66ff674e9aa237b0a207e00e53
EDGE-qy <113410981+EDGE-qy@users.noreply.github.com>
2023-01-05 21:40:35 +0800

    [New Unit] Three Body Man (#49)

--- src/battle_game/core/obstacles/obstacles.h
+++ src/battle_game/core/obstacles/obstacles.h
@@ -1,2 +1,3 @@
 #pragma once
 #include "battle_game/core/obstacles/block.h"
+#include "battle_game/core/obstacles/safety_declaration.h"
--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -2,4 +2,5 @@
 #include "battle_game/core/units/Whaooooo_tank.h"
 #include "battle_game/core/units/double_scatter_tank.h"
 #include "battle_game/core/units/inferno_tank.h"
+#include "battle_game/core/units/three_body_man.h"
 #include "battle_game/core/units/tiny_tank.h"
