31bda25fb184a78dfb143b06338b6f8101ededa0
XuGW-Kevin <xugw22@mails.tsinghua.edu.cn>
2023-01-05 17:43:29 +0800

    remove_comment

--- src/battle_game/core/bullets/rocket.cpp
+++ src/battle_game/core/bullets/rocket.cpp
@@ -36,7 +36,6 @@ Rocket::Rocket(GameCore *core,
 
 void Rocket::Render() {
   SetTransformation(position_, rotation_, glm::vec2{1.0f});
-  // SetColor(game_core_->GetPlayerColor(player_id_));
   SetTexture("../../textures/rocket.png");
   DrawModel(0);
 }
