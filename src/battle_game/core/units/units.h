4e31868daa42d70bc6a9247c44e62ebbec27c046
AAA <3225329724@qq.com>
2023-01-05 18:52:42 +0800

    Whaooooo_tank

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "battle_game/core/units/Whaooooo_tank.h"
 #include "battle_game/core/units/double_scatter_tank.h"
 #include "battle_game/core/units/inferno_tank.h"
 #include "battle_game/core/units/tiny_tank.h"
