4e9cb80dfc2370fa431175e3963dd69956467e75
the-bucket-rider <61498107+the-bucket-rider@users.noreply.github.com>
2023-01-06 12:52:14 +0800

    [Unit]  Triple shot tank (#81)

---
+++ src/battle_game/core/bullets/split_bullet.cpp
@@ -0,0 +1,63 @@
+//
+// Created by 13552 on 2023/1/8.
+//
+#pragma once
+#include <iostream>
+#include "battle_game/core/bullet.h"
+#include "battle_game/core/bullets/cannon_ball.h"
+#include "split_bullet.h"
+#include "battle_game/core/game_core.h"
+#include "battle_game/core/particles/particles.h"
+namespace battle_game::bullet{
+    split_bullet::split_bullet(GameCore *core, uint32_t id, uint32_t unit_id, uint32_t player_id, glm::vec2 position,
+                               float rotation, float damage_scale, glm::vec2 velocity) : Bullet(core, id, unit_id, player_id, position, rotation, damage_scale),
+                                                                                         velocity_(velocity){split_countdown = 10;}
+    void split_bullet::Update() {
+        split_countdown--;
+        //std::cout << split_countdown << std::endl;
+        position_ += velocity_ * kSecondPerTick;
+        bool should_die = false;
+        if (game_core_->IsBlockedByObstacles(position_)) {
+            should_die = true;
+        }
+        auto &units = game_core_->GetUnits();
+        for (auto &unit : units) {
+            if (unit.first == player_id_) {
+                continue;
+            }
+            if (unit.second->IsHit(position_)) {
+                game_core_->PushEventDealDamage(unit.first, id_, damage_scale_ * 10.0f);
+                should_die = true;
+            }
+            if(split_countdown == 0)
+            {
+                for(int i = -2; i <= 2; i++) {
+                    game_core_->PushEventGenerateBullet<bullet::CannonBall>(unit_id_, player_id_, position_, rotation_,
+                                                                            damage_scale_, Rotate(velocity_, float(i*1.0/10)));
+                }
+                should_die = true;
+            }
+        }
+
+        if (should_die) {
+            game_core_->PushEventRemoveBullet(id_);
+        }
+    }
+    void split_bullet::Render() {
+        SetTransformation(position_, rotation_, glm::vec2{0.1f});
+        SetColor(glm::vec4{1.0f, 0.0f, 0.0f, 0.4f});
+        SetScale(glm::vec2{0.15f, 0.15f});
+        SetTexture("../../textures/particle3.png");
+        DrawModel(0);
+    }
+
+    split_bullet::~split_bullet() noexcept {
+        if(split_countdown) {
+            for (int i = 0; i < 5; i++) {
+                game_core_->PushEventGenerateParticle<particle::Smoke>(
+                        position_, rotation_, game_core_->RandomInCircle() * 2.0f, 0.2f,
+                        glm::vec4{0.0f, 0.0f, 0.0f, 1.0f}, 3.0f);
+            }
+        }
+    }
+}
--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -5,3 +5,5 @@
 #include "battle_game/core/units/tank_xxy.h"
 #include "battle_game/core/units/three_body_man.h"
 #include "battle_game/core/units/tiny_tank.h"
+#include "battle_game/core/units/triple_shot_tank.h"
+#include "battle_game/core/units/Split_bullet_cannon.h"
