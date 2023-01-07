c4c46dcc383a4a196b79c999167e4508900138b0
unprintable <unprintable123@gmail.com>
2023-01-08 00:25:27 +0800

    code reuse

--- src/battle_game/core/units/missile_tank.h
+++ src/battle_game/core/units/missile_tank.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "battle_game/core/unit.h"
+#include "battle_game/core/units/tiny_tank.h"
 
 namespace battle_game::unit {
-class MissileTank : public Unit {
+class MissileTank : public Tank {
  public:
   MissileTank(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
@@ -10,13 +11,8 @@ class MissileTank : public Unit {
   [[nodiscard]] bool IsHit(glm::vec2 position) const override;
 
  protected:
-  void TankMove(float move_speed, float rotate_angular_speed);
-  void TurretRotate();
   void Fire();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
-
-  float turret_rotation_{0.0f};
-  uint32_t fire_count_down_{0};
 };
 }  // namespace battle_game::unit
