9b97e073645093aa64fc06844e1b32e0e64cbf75
jojobananana <1263265504@qq.com>
2023-01-08 14:29:30 +0800

    New unit jojo

--- src/battle_game/core/bullets/bullets.h
+++ src/battle_game/core/bullets/bullets.h
@@ -5,3 +5,5 @@
 #include "battle_game/core/bullets/rocket.h"
 #include "battle_game/core/bullets/warning_line.h"
 #include "battle_game/core/bullets/water_drop.h"
+#include "battle_game/core/bullets/basketball.h"
+#include "battle_game/core/bullets/chicken.h"
