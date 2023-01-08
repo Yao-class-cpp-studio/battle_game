4366ccd349ed1adc73c06ad03d980be908376a97
ljc1301 <ljc1301@126.com>
2023-01-08 14:02:45 +0800

    [New unit] add unit Smoke Bomb Tank

--- src/battle_game/core/bullets/bullets.h
+++ src/battle_game/core/bullets/bullets.h
@@ -2,5 +2,6 @@
 
 #include "battle_game/core/bullets/cannon_ball.h"
 #include "battle_game/core/bullets/rocket.h"
+#include "battle_game/core/bullets/smoke_bomb.h"
 #include "battle_game/core/bullets/warning_line.h"
 #include "battle_game/core/bullets/water_drop.h"
