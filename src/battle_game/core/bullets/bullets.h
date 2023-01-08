15a0449a2f5c89402bdafeb5049087e07cc468cb
胡宸源 <cy-hu22@mails.tsinghua.edu.cn>
2023-01-08 15:19:56 +0800

    updateall

--- src/battle_game/core/bullets/bullets.h
+++ src/battle_game/core/bullets/bullets.h
@@ -3,6 +3,6 @@
 #include "battle_game/core/bullets/cannon_ball.h"
 #include "battle_game/core/bullets/missile.h"
 #include "battle_game/core/bullets/rocket.h"
+#include "battle_game/core/bullets/split_bullet.h"
 #include "battle_game/core/bullets/warning_line.h"
 #include "battle_game/core/bullets/water_drop.h"
-#include "battle_game/core/bullets/split_bullet.h"
