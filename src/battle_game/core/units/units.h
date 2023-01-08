7f06ae961b701563bc72565aff893b24252f1954
wanghm22 <113539355+wanghm22@users.noreply.github.com>
2023-01-08 16:38:49 +0800

    battle

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -8,3 +8,4 @@
 #include "battle_game/core/units/three_body_man.h"
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/triple_shot_tank.h"
+#include "battle_game/core/units/wanghm_tank.h"
