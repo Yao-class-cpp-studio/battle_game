be07c4417ce540fd95e7ecd567803bd56384a3ac
王梓人 <wang-zr22@mails.tsinghua.edu.cn>
2023-01-08 23:04:06 +0800

    1

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "battle_game/core/units/Whaooooo_tank.h"
 #include "battle_game/core/units/double_scatter_tank.h"
+#include "battle_game/core/units/elementary_tank.h"
 #include "battle_game/core/units/inferno_tank.h"
 #include "battle_game/core/units/missile_tank.h"
 #include "battle_game/core/units/square_tank.h"
@@ -8,4 +9,3 @@
 #include "battle_game/core/units/three_body_man.h"
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/triple_shot_tank.h"
-#include "battle_game/core/units/elementary_tank.h"
