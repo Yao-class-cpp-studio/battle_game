76a081c83066ee3447b08dfa886a17a63b9312d6
王梓人 <wang-zr22@mails.tsinghua.edu.cn>
2023-01-08 22:53:00 +0800

    new unit element_tank

---
+++ src/battle_game/core/units/elementary_tank.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "battle_game/core/unit.h"
+
+namespace battle_game::unit {
+class ElementaryTank : public Unit {
+ public:
+  ElementaryTank(GameCore *game_core, uint32_t id, uint32_t player_id);
+  void Render() override;
+  void Update() override;
+  [[nodiscard]] bool IsHit(glm::vec2 position) const override;
+
+ protected:
+  void TankMove(float move_speed, float rotate_angular_speed);
+  void TurretRotate();
+  void Fire();
+  void Accelerate();
+  [[nodiscard]] const char *UnitName() const override;
+  [[nodiscard]] const char *Author() const override;
+
+  float turret_rotation_{0.0f};
+  uint32_t fire_count_down_{0};
+  bool accelerate_flag = FALSE;
+  int accelerate_time;
+  int num_strong_bullets = 0;
+};
+}  // namespace battle_game::unit
