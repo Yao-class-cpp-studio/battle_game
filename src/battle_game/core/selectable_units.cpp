86470b8ea8e03877e83065b91c02d4cb46d7055a
ljc1301 <ljc1301@126.com>
2023-01-09 20:22:09 +0800

    [Unit] Smoke bomb tank (#139)

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -42,6 +42,7 @@ void GameCore::GeneratePrimaryUnitList() {
   ADD_SELECTABLE_UNIT(unit::SquareTank);
   ADD_SELECTABLE_UNIT(unit::ZibengDog);
   ADD_SELECTABLE_UNIT(unit::RageTank);
+  ADD_SELECTABLE_UNIT(unit::SmokeBombTank);
 
   unit.reset();
 }
