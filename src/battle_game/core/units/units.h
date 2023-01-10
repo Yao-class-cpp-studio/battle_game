d9767d190200b458b357eb48c5f81889d5a8de57
jk21lcl <748630503@qq.com>
2023-01-10 16:46:25 +0800

    Update units.h

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -15,5 +15,4 @@
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/triple_shot_tank.h"
 #include "battle_game/core/units/zibeng_dog.h"
-#include "battle_game/core/units/rage_tank_yangyr.h"
 #include "battle_game/core/units/heal_tank.h"
