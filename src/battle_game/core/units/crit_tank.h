279cd89502bbe89b6c4e177ffcabf5708af08206
jinchen-jiang <jinchen-jiang@outlook.com>
2023-01-11 11:16:07 +0800

    Fix nit

--- src/battle_game/core/units/crit_tank.h
+++ src/battle_game/core/units/crit_tank.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "battle_game/core/unit.h"
+#include "battle_game/core/units/tiny_tank.h"
 
 namespace battle_game::unit {
-class CritTank : public Unit {
+class CritTank : public Tank {
  public:
   CritTank(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
@@ -10,14 +11,11 @@ class CritTank : public Unit {
   [[nodiscard]] bool IsHit(glm::vec2 position) const override;
 
  protected:
-  void TankMove(float move_speed, float rotate_angular_speed);
-  void TurretRotate();
   void Fire();
   void BlessingsOfTheAbyssalMoon();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
 
-  float turret_rotation_{0.0f};
   uint32_t fire_count_down_{0};
   uint32_t blessings_count_down_{0};
   uint32_t blessings_duration_{0};
