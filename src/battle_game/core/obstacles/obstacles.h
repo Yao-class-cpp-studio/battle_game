16dd8b15fdf72b05d5749d1c31d799ec66a154dc
hwk22 <113423708+hwk22@users.noreply.github.com>
2023-01-12 15:04:13 +0800

    Update obstacles.h

--- src/battle_game/core/obstacles/obstacles.h
+++ src/battle_game/core/obstacles/obstacles.h
@@ -1,4 +1,5 @@
 #pragma once
 #include "battle_game/core/obstacles/block.h"
 #include "battle_game/core/obstacles/rebounding_block.h"
+#include "battle_game/core/obstacles/repli_block.h"
 #include "battle_game/core/obstacles/safety_declaration.h"
