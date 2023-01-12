91841b2813ec17db85084365dc8c1f2d002678cc
hwk22 <113423708+hwk22@users.noreply.github.com>
2023-01-12 15:03:38 +0800

    Add files via upload

---
+++ src/battle_game/core/obstacles/repli_block.cpp
@@ -0,0 +1,25 @@
+#include "battle_game/core/obstacles/repli_block.h"
+
+namespace battle_game::obstacle {
+
+RepliBlock::RepliBlock(GameCore *game_core,
+                       uint32_t id,
+                       glm::vec2 position,
+                       float rotation,
+                       glm::vec2 scale)
+    : Obstacle(game_core, id, position, rotation) {
+}
+
+bool RepliBlock::IsBlocked(glm::vec2 p) const {
+  p = WorldToLocal(p);
+  return p.x <= scale_.x && p.x >= -scale_.x && p.y <= scale_.y &&
+         p.y >= -scale_.y;
+}
+
+void RepliBlock::Render() {
+  battle_game::SetColor(glm::vec4{0.0f, 0.0f, 0.0f, 1.0f});
+  battle_game::SetTexture(0);
+  battle_game::SetTransformation(position_, rotation_, scale_);
+  battle_game::DrawModel(0);
+}
+}  // namespace battle_game::obstacle
