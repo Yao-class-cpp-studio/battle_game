612af1ffb7d95f60bfc559fef3e7ba966f1154ef
ProductiveProfessionalProgrammer <wang-yi22@mails.tsinghua.edu.cn>
2023-01-12 11:51:49 +0800

    Optimize implementation of Sparky

--- src/battle_game/core/units/sparky.cpp
+++ src/battle_game/core/units/sparky.cpp
@@ -12,7 +12,7 @@ uint32_t sparky_turret_model_index = 0xffffffffu;
 }  // namespace
 
 Sparky::Sparky(GameCore *game_core, uint32_t id, uint32_t player_id)
-    : Unit(game_core, id, player_id) {
+    : Tank(game_core, id, player_id) {
   if (!~sparky_body_model_index) {
     auto mgr = AssetsManager::GetInstance();
     {
@@ -77,42 +77,11 @@ void Sparky::Render() {
 }
 
 void Sparky::Update() {
-  SparkyMove(3.0f, glm::radians(180.0f));
+  TankMove(3.0f, glm::radians(180.0f));
   TurretRotate();
   Fire();
 }
 
-void Sparky::SparkyMove(float move_speed, float rotate_angular_speed) {
-  auto player = game_core_->GetPlayer(player_id_);
-  if (player) {
-    auto &input_data = player->GetInputData();
-    glm::vec2 offset{0.0f};
-    if (input_data.key_down[GLFW_KEY_W]) {
-      offset.y += 1.0f;
-    }
-    if (input_data.key_down[GLFW_KEY_S]) {
-      offset.y -= 1.0f;
-    }
-    float speed = move_speed * GetSpeedScale();
-    offset *= kSecondPerTick * speed;
-    auto new_position =
-        position_ + glm::vec2{glm::rotate(glm::mat4{1.0f}, rotation_,
-                                          glm::vec3{0.0f, 0.0f, 1.0f}) *
-                              glm::vec4{offset, 0.0f, 0.0f}};
-    if (!game_core_->IsBlockedByObstacles(new_position)) {
-      game_core_->PushEventMoveUnit(id_, new_position);
-    }
-    float rotation_offset = 0.0f;
-    if (input_data.key_down[GLFW_KEY_A]) {
-      rotation_offset += 1.0f;
-    }
-    if (input_data.key_down[GLFW_KEY_D]) {
-      rotation_offset -= 1.0f;
-    }
-    rotation_offset *= kSecondPerTick * rotate_angular_speed * GetSpeedScale();
-    game_core_->PushEventRotateUnit(id_, rotation_ + rotation_offset);
-  }
-}
 
 void Sparky::TurretRotate() {
   auto player = game_core_->GetPlayer(player_id_);
--- src/battle_game/core/units/sparky.h
+++ src/battle_game/core/units/sparky.h
@@ -1,19 +1,20 @@
 #pragma once
 #include "battle_game/core/unit.h"
 
+#include "battle_game/core/units/tiny_tank.h"
+
 namespace battle_game::unit {
-class Sparky : public Unit {
+class Sparky : public Tank {
  public:
   Sparky(GameCore *game_core, uint32_t id, uint32_t player_id);
-  void Render() override;
-  void Update() override;
-  float GetDamageScale() const override;
-  float GetHealthScale() const override;
+  void Render();
+  void Update();
+  float GetDamageScale() const;
+  float GetHealthScale() const;
   float GetSpeedScale() const override;
   [[nodiscard]] bool IsHit(glm::vec2 position) const override;
 
  protected:
-  void SparkyMove(float move_speed, float rotate_angular_speed);
   void TurretRotate();
   void Fire();
   [[nodiscard]] const char *UnitName() const override;
