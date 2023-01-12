2e0882996a73ea5dd2bb7877af663c91a6222a98
ZigZagKmp <36724356+ZigZagKmp@users.noreply.github.com>
2023-01-12 23:49:26 +0800

    Update selectable_units.cpp

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -29,7 +29,6 @@ void GameCore::GeneratePrimaryUnitList() {
   /*
    * TODO: Add Your Unit Here!
    * */
-  
   ADD_SELECTABLE_UNIT(unit::Tank);
   ADD_SELECTABLE_UNIT(unit::InfernoTank);
   ADD_SELECTABLE_UNIT(unit::RoundUFO);
