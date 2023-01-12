1fae18fa9b847686e2bc0496839bce4daad61926
jk21lcl <113412883+jk21lcl@users.noreply.github.com>
2023-01-12 23:16:00 +0800

    Change the order of selectable units (#214)

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -29,11 +29,11 @@ void GameCore::GeneratePrimaryUnitList() {
   /*
    * TODO: Add Your Unit Here!
    * */
+  ADD_SELECTABLE_UNIT(unit::InfernoTank);
   ADD_SELECTABLE_UNIT(unit::RoundUFO);
   ADD_SELECTABLE_UNIT(unit::Tank);
   ADD_SELECTABLE_UNIT(unit::DoubleScatterTank);
   ADD_SELECTABLE_UNIT(unit::ThreeBodyMan);
-  ADD_SELECTABLE_UNIT(unit::InfernoTank);
   ADD_SELECTABLE_UNIT(unit::MissileTank);
   ADD_SELECTABLE_UNIT(unit::TripleShotTank);
   ADD_SELECTABLE_UNIT(unit::TankXxy);
