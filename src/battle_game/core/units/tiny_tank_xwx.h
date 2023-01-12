1cbadea8b48324f822c84f5498de95c3069f78a8
teddy <teddy@teddydeMacBook-Pro.local>
2023-01-12 09:28:08 +0800

    fixed

--- src/battle_game/core/units/tiny_tank_xwx.h
+++ src/battle_game/core/units/tiny_tank_xwx.h
@@ -1,17 +1,15 @@
 #pragma once
 #include "battle_game/core/unit.h"
-
+#include "battle_game/core/units/tiny_tank.h"
 namespace battle_game::unit {
-class Tank_xwx : public Unit {
+class Tank_xwx : public Tank {
  public:
   Tank_xwx(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
   void Update() override;
-
   [[nodiscard]] bool IsHit(glm::vec2 position) const override;
+
  protected:
-  void TankMove(float move_speed, float rotate_angular_speed);
-  void TurretRotate();
   void Fire();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
