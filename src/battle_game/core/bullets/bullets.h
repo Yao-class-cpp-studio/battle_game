eed6650a0d7f645ced54bb2c8357f05a444d9bfe
ProductiveProfessionalProgrammer <wang-yi22@mails.tsinghua.edu.cn>
2023-01-12 21:41:38 +0800

    fix clang-format issues

--- src/battle_game/core/bullets/bullets.h
+++ src/battle_game/core/bullets/bullets.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "battle_game/core/bullets/cannon_ball.h"
 #include "battle_game/core/bullets/crit_bullet.h"
+#include "battle_game/core/bullets/electric_ball.h"
 #include "battle_game/core/bullets/mine.h"
 #include "battle_game/core/bullets/missile.h"
 #include "battle_game/core/bullets/rebounding_ball.h"
@@ -9,4 +10,3 @@
 #include "battle_game/core/bullets/sweaty_soybean.h"
 #include "battle_game/core/bullets/warning_line.h"
 #include "battle_game/core/bullets/water_drop.h"
-#include "battle_game/core/bullets/electric_ball.h"
