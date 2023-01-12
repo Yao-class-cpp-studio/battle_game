d643be96bb75df516a1f738d3f0123fcaf9ab880
wmj2022 <113516461+wmj2022@users.noreply.github.com>
2023-01-12 21:43:41 +0800

    Update wmj_tank.h

--- src/battle_game/core/units/wmj_tank.h
+++ src/battle_game/core/units/wmj_tank.h
@@ -1,22 +1,21 @@
 #pragma once
 #include "battle_game/core/unit.h"
+#include "battle_game/core/units/tiny_tank.h"
+
 
 namespace battle_game::unit {
-class wmj_Tank : public Unit {
+class wmjTank : public Tank {
  public:
-  wmj_Tank(GameCore *game_core, uint32_t id, uint32_t player_id);
+  wmjTank(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
   void Update() override;
-  [[nodiscard]] bool IsHit(glm::vec2 position) const override;
+
 
  protected:
-  void TankMove(float move_speed, float rotate_angular_speed);
-  void TurretRotate();
   void Fire();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
 
-  float turret_rotation_{0.0f};
-  uint32_t fire_count_down_{0};
+
 };
 }  // namespace battle_game::unit
