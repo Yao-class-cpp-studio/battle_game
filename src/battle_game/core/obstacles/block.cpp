8a2c5ea69c1bef5056e89319769fb3e4be2c56b8
ZigZagKmp <2123399800@qq.com>
2023-01-12 16:18:17 +0800

    Obstacle Health system Done!

--- src/battle_game/core/obstacles/block.cpp
+++ src/battle_game/core/obstacles/block.cpp
@@ -8,7 +8,6 @@ Block::Block(GameCore *game_core,
              float rotation,
              glm::vec2 scale)
     : Obstacle(game_core, id, position, rotation) {
-  SetDestructible();
 }
 
 bool Block::IsBlocked(glm::vec2 p) const {
