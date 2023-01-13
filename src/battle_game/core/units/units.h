bed4509094f41b4f00c6811e05fe5f4c93666bf0
geometry2021 <2912035963@qq.com>
2023-01-13 22:42:26 +0800

    new unit by geometry2021

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -21,4 +21,3 @@
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/triple_shot_tank.h"
 #include "battle_game/core/units/zibeng_dog.h"
-#include "battle_game/core/units/geometry2021_plane.h"
