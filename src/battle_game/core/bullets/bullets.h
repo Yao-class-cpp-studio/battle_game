9790675923a493d592b30733bf4c54f656f35154
LMOliver <31990902+LMOliver@users.noreply.github.com>
2023-01-13 23:28:11 +0800

    [Unit] LMTank and debug (#226)

--- src/battle_game/core/bullets/bullets.h
+++ src/battle_game/core/bullets/bullets.h
@@ -3,6 +3,7 @@
 #include "battle_game/core/bullets/coin.h"
 #include "battle_game/core/bullets/crit_bullet.h"
 #include "battle_game/core/bullets/electric_ball.h"
+#include "battle_game/core/bullets/energy_beam.h"
 #include "battle_game/core/bullets/mine.h"
 #include "battle_game/core/bullets/missile.h"
 #include "battle_game/core/bullets/rebounding_ball.h"
