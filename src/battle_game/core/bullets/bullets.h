7b112d1e828661f1e58660a28944a061aefdc79c
cylic14790382 <liuyinch22@mails.tsinghua.edu.cn>
2023-01-13 22:38:13 +0800

    still clang problem ....

--- src/battle_game/core/bullets/bullets.h
+++ src/battle_game/core/bullets/bullets.h
@@ -8,7 +8,7 @@
 #include "battle_game/core/bullets/rebounding_ball.h"
 #include "battle_game/core/bullets/rocket.h"
 #include "battle_game/core/bullets/smoke_bomb.h"
+#include "battle_game/core/bullets/snowball.h"
 #include "battle_game/core/bullets/sweaty_soybean.h"
 #include "battle_game/core/bullets/warning_line.h"
 #include "battle_game/core/bullets/water_drop.h"
-#include "battle_game/core/bullets/snowball.h"
