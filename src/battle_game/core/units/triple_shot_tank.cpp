1047abfa583156c73a71440e4fcfd93d1238338c
yzc0731 <1360187480@qq.com>
2023-01-13 20:38:29 +0800

    restore triple_shot bug

--- src/battle_game/core/units/triple_shot_tank.cpp
+++ src/battle_game/core/units/triple_shot_tank.cpp
@@ -22,25 +22,40 @@ void TripleShotTank::Update() {
   TripleFire();
 }
 
+///////////////////////////////////////////////
+////////////////Restore Bug////////////////////
+///////////////////////////////////////////////
+
 void TripleShotTank::TripleFire() {
-  if (fire_count_down_ != 0 && fire_count_down_ != 3 && fire_count_down_ != 6) {
-    fire_count_down_--;
-  } else {
+  if (fire_count_down_ == 0) {
     auto player = game_core_->GetPlayer(player_id_);
     if (player) {
       auto &input_data = player->GetInputData();
       if (input_data.mouse_button_down[GLFW_MOUSE_BUTTON_LEFT]) {
         auto velocity = Rotate(glm::vec2{0.0f, 20.0f}, turret_rotation_);
-        GenerateBullet<bullet::CannonBall>(
-            position_ + Rotate({0.0f, 1.5f}, turret_rotation_),
-            turret_rotation_, GetDamageScale(), velocity);
-        if (fire_count_down_ == 0) {
-          fire_count_down_ = kTickPerSecond;  // Fire interval 1 second.
-        } else {
-          fire_count_down_--;
-        }
+        glm::vec2 position =
+            position_ + Rotate(glm::vec2{0.0f, 1.5f}, turret_rotation_);
+        float rotation = turret_rotation_;
+        float damage_scale = GetDamageScale();
+        GenerateBullet<bullet::CannonBall>(position, rotation, damage_scale,
+                                           velocity);
+        fire_count_down_ = kTickPerSecond;
       }
     }
+  } else if (fire_count_down_ == 58 || fire_count_down_ == 55) {
+    auto player = game_core_->GetPlayer(player_id_);
+    if (player) {
+      auto velocity = Rotate(glm::vec2{0.0f, 20.0f}, turret_rotation_);
+      glm::vec2 position =
+          position_ + Rotate(glm::vec2{0.0f, 1.5f}, turret_rotation_);
+      float rotation = turret_rotation_;
+      float damage_scale = GetDamageScale();
+      GenerateBullet<bullet::CannonBall>(position, rotation, damage_scale,
+                                         velocity);
+    }
+    fire_count_down_--;
+  } else {
+    fire_count_down_--;
   }
 }
 
