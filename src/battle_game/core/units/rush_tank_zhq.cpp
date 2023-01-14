5686da84d5299a196bb1155675a8573e2d5e6535
zhqwqwq <1521480316@qq.com>
2023-01-14 18:16:38 +0800

    format

--- src/battle_game/core/units/rush_tank_zhq.cpp
+++ src/battle_game/core/units/rush_tank_zhq.cpp
@@ -1,7 +1,7 @@
+#include <ctime>
 #include "battle_game/core/bullets/bullets.h"
 #include "battle_game/core/game_core.h"
 #include "battle_game/graphics/graphics.h"
-#include <ctime>
 
 namespace battle_game::unit {
 
