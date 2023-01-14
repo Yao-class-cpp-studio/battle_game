515523e638eaf5bcbf1c82ac113751fe05e7da37
horipse01 <2424992568@qq.com>
2023-01-14 10:40:54 +0800

    new buff

--- src/battle_game/core/bullets/stunning.cpp
+++ src/battle_game/core/bullets/stunning.cpp
@@ -4,26 +4,26 @@
 #include "battle_game/core/particles/particles.h"
 
 namespace battle_game::bullet {
-HESH::HESH(GameCore *core,
-           uint32_t id,
-           uint32_t unit_id,
-           uint32_t player_id,
-           glm::vec2 position,
-           float rotation,
-           float damage_scale,
-           glm::vec2 velocity)
+Stunning::Stunning(GameCore *core,
+                   uint32_t id,
+                   uint32_t unit_id,
+                   uint32_t player_id,
+                   glm::vec2 position,
+                   float rotation,
+                   float damage_scale,
+                   glm::vec2 velocity)
     : Bullet(core, id, unit_id, player_id, position, rotation, damage_scale),
       velocity_(velocity) {
 }
 
-void HESH::Render() {
+void Stunning::Render() {
   SetTransformation(position_, rotation_, glm::vec2{0.1f});
   SetColor({1.0f, 0.0f, 0.0f, 1.0f});
   SetTexture("../../textures/particle3.png");
   DrawModel(0);
 }
 
-void HESH::Update() {
+void Stunning::Update() {
   position_ += velocity_ * kSecondPerTick;
   bool should_die = false;
   if (game_core_->IsBlockedByObstacles(position_)) {
@@ -48,7 +48,7 @@ void HESH::Update() {
   }
 }
 
-HESH::~HESH() {
+Stunning::~Stunning() {
   for (int i = 0; i < 5; i++) {
     game_core_->PushEventGenerateParticle<particle::Smoke>(
         position_, rotation_, game_core_->RandomInCircle() * 2.0f, 0.2f,
--- src/battle_game/core/bullets/stunning.h
+++ src/battle_game/core/bullets/stunning.h
@@ -2,17 +2,17 @@
 #include "battle_game/core/bullet.h"
 
 namespace battle_game::bullet {
-class HESH : public Bullet {
+class Stunning : public Bullet {
  public:
-  HESH(GameCore *core,
-       uint32_t id,
-       uint32_t unit_id,
-       uint32_t player_id,
-       glm::vec2 position,
-       float rotation,
-       float damage_scale,
-       glm::vec2 velocity);
-  ~HESH() override;
+  Stunning(GameCore *core,
+           uint32_t id,
+           uint32_t unit_id,
+           uint32_t player_id,
+           glm::vec2 position,
+           float rotation,
+           float damage_scale,
+           glm::vec2 velocity);
+  ~Stunning() override;
   void Render() override;
   void Update() override;
 
