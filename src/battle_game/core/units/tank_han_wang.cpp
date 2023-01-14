cf89f14c08eef9a8f013a7cac8fe04408baf2260
han-wang19 <whdyx0101@163.com>
2023-01-14 21:29:03 +0800

    Format

--- src/battle_game/core/units/tank_han_wang.cpp
+++ src/battle_game/core/units/tank_han_wang.cpp
@@ -1,21 +1,23 @@
 #include "battle_game/core/units/tank_han_wang.h"
-#include "tiny_tank.h"
 
 #include "battle_game/core/bullets/bullets.h"
 #include "battle_game/core/game_core.h"
 #include "battle_game/graphics/graphics.h"
+#include "tiny_tank.h"
+// Format.
 
 namespace battle_game::unit {
-    
-TankHanWang::TankHanWang(GameCore *game_core, uint32_t id, uint32_t player_id) : Tank(game_core, id, player_id) {
+
+TankHanWang::TankHanWang(GameCore *game_core, uint32_t id, uint32_t player_id)
+    : Tank(game_core, id, player_id) {
 }
 
 const char *TankHanWang::UnitName() const {
-    return "Tank Han Wang";
+  return "Tank Han Wang";
 }
 
 const char *TankHanWang::Author() const {
-    return "Han Wang";
+  return "Han Wang";
 }
 
-}
+}  // namespace battle_game::unit
