167dbdcd62179686cb82e88f38d80163a8ee3c83
zhqwqwq <113563900+zhqwqwq@users.noreply.github.com>
2023-01-14 21:45:47 +0800

    Update shadow_tank_zhq.cpp

--- src/battle_game/core/units/shadow_tank_zhq.cpp
+++ src/battle_game/core/units/shadow_tank_zhq.cpp
@@ -1,7 +1,4 @@
-#include "battle_game/core/bullets/bullets.h"
 #include "battle_game/core/game_core.h"
-#include "battle_game/core/units/double_scatter_tank.h"
-#include "battle_game/graphics/graphics.h"
 
 namespace battle_game::unit {
 
