73716c766438fa964e11361d56352846c3d99803
liyitang22 <113581917+liyitang22@users.noreply.github.com>
2023-01-14 10:57:15 +0800

    Add selectable tank

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -53,7 +53,8 @@ void GameCore::GeneratePrimaryUnitList() {
   ADD_SELECTABLE_UNIT(unit::CritTank);
   ADD_SELECTABLE_UNIT(unit::Railgun);
   ADD_SELECTABLE_UNIT(unit::Udongein);
-
+  ADD_SELECTABLE_UNIT(unit::tangtang_Tank);
+  
   unit.reset();
 }
 }  // namespace battle_game
