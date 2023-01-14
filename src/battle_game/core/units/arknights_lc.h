47fb033788bdca213d5699546e5a3e5573d466c3
liuchuan22 <liuchuan22@mails.tsinghua.edu.cn>
2023-01-14 22:30:36 +0800

    finish_ten_point_task

---
+++ src/battle_game/core/units/arknights_lc.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "battle_game/core/unit.h"
+
+namespace battle_game::unit {
+class Arknights : public Unit {
+ public:
+  Arknights(GameCore *game_core, uint32_t id, uint32_t player_id);
+  void Render() override;
+  void Update() override;
+  float GetSpeedScale() const override;
+  float GetHealthScale() const override;
+  [[nodiscard]] bool IsHit(glm::vec2 position) const override;
+
+ protected:
+  void ArknightsMove(float move_speed, float rotate_angular_speed);
+  void TurretRotate();
+  void Fire();
+  [[nodiscard]] const char *UnitName() const override;
+  [[nodiscard]] const char *Author() const override;
+
+  float turret_rotation_{0.0f};
+  uint32_t fire_count_down_{0};
+  uint32_t mine_count_down_{0};
+};
+}  // namespace battle_game::unit
