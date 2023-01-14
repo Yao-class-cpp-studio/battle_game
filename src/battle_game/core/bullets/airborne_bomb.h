6a80e6ed3823b4fadb498929e61b674ec63ee8e8
C161324 <13910065957@139.com>
2023-01-14 13:16:25 +0800

    [New unit&various utilities and effects]txyC161324

--- src/battle_game/core/bullets/airborne_bomb.h
+++ src/battle_game/core/bullets/airborne_bomb.h
@@ -4,13 +4,13 @@ namespace battle_game::bullet {
 class AirborneBomb : public Bullet {
  public:
   AirborneBomb(GameCore *core,
-              uint32_t id,
-              uint32_t unit_id,
-              uint32_t player_id,
-              glm::vec2 position,
-              float rotation,
-              float damage_scale,
-              glm::vec2 velocity);
+               uint32_t id,
+               uint32_t unit_id,
+               uint32_t player_id,
+               glm::vec2 position,
+               float rotation,
+               float damage_scale,
+               glm::vec2 velocity);
   ~AirborneBomb() override;
   void Render() override;
   void Update() override;
