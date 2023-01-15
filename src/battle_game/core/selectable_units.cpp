363f6c18e7ee7c49b07d2284394a9faa5862cd5a
pangker7 <myh13671173270@163.com>
2023-01-15 16:39:43 +0800

    conflict 4

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -49,8 +49,8 @@ void GameCore::GeneratePrimaryUnitList() {
   ADD_SELECTABLE_UNIT(unit::SmokeBombTank);
   ADD_SELECTABLE_UNIT(unit::Sparky);
   ADD_SELECTABLE_UNIT(unit::CritTank);
-  ADD_SELECTABLE_UNIT(unit::Railgun);
   ADD_SELECTABLE_UNIT(unit::LaserTank);
+  ADD_SELECTABLE_UNIT(unit::Railgun);
   ADD_SELECTABLE_UNIT(unit::Udongein);
 
   unit.reset();
