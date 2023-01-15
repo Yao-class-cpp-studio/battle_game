4805d25ef51fe5556a2edfad8a22be936b7aa0fb
starusc <starusc@qq.com>
2023-01-15 21:11:38 +0800

    pre—commit

--- src/battle_game/core/units/TankStarusc.cpp
+++ src/battle_game/core/units/TankStarusc.cpp
@@ -1,17 +1,18 @@
 #include "TankStarusc.h"
 
+#include <iostream>
+
 #include "battle_game/core/bullets/bullets.h"
 #include "battle_game/core/game_core.h"
 #include "battle_game/graphics/graphics.h"
 
-#include <iostream>
-
 namespace battle_game::unit {
 
-const float pi=acos(-1.0);
+const float pi = acos(-1.0);
 
 TankStarusc::TankStarusc(GameCore *game_core, uint32_t id, uint32_t player_id)
-    : Tank(game_core, id, player_id) { }
+    : Tank(game_core, id, player_id) {
+}
 
 void TankStarusc::Render() {
   Tank::Render();
@@ -27,20 +28,20 @@ void TankStarusc::Update() {
 
 void TankStarusc::Fire() {
   // 回旋子弹
-  float theta = pi*2/kTickPerSecond*mine_count_;
+  float theta = pi * 2 / kTickPerSecond * mine_count_;
   auto velocity = Rotate(glm::vec2{0.0f, 20.0f}, theta);
-  GenerateBullet<bullet::CannonBall>(
-    position_ + Rotate({0.0f,1.2f}, theta),
-    theta, GetDamageScale()/10, velocity);
+  GenerateBullet<bullet::CannonBall>(position_ + Rotate({0.0f, 1.2f}, theta),
+                                     theta, GetDamageScale() / 10, velocity);
   ++mine_count_;
-  if(mine_count_ >= kTickPerSecond) mine_count_ = 0;
-  
+  if (mine_count_ >= kTickPerSecond)
+    mine_count_ = 0;
+
   if (fire_count_down_ == 0) {
     auto player = game_core_->GetPlayer(player_id_);
     if (player) {
       auto &input_data = player->GetInputData();
       if (input_data.mouse_button_down[GLFW_MOUSE_BUTTON_LEFT]) {
-        fire_count_down_ = kTickPerSecond/2;  // Fire interval 1 second.
+        fire_count_down_ = kTickPerSecond / 2;  // Fire interval 1 second.
         fire_theta_ = turret_rotation_;
         fire_count_ = 3;
       }
@@ -50,12 +51,12 @@ void TankStarusc::Fire() {
     fire_count_down_--;
   }
 
-  if(fire_count_ > 0){
+  if (fire_count_ > 0) {
     --fire_count_;
     velocity = Rotate(glm::vec2{0.0f, 20.0f}, fire_theta_);
     GenerateBullet<bullet::CannonBall>(
-        position_ + Rotate({0.0f, 1.2f}, fire_theta_),
-        fire_theta_, GetDamageScale(), velocity);
+        position_ + Rotate({0.0f, 1.2f}, fire_theta_), fire_theta_,
+        GetDamageScale(), velocity);
   }
 }
 
