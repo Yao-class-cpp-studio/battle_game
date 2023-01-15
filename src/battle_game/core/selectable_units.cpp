becf7827ccab960e06c164d0a2d563a0a7029d70
csccsru <113533448+csccsru@users.noreply.github.com>
2023-01-15 12:40:26 +0800

    make a new tank unit called phantom_of_cosin which can avoid first hit.

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -44,7 +44,7 @@ void GameCore::GeneratePrimaryUnitList() {
   ADD_SELECTABLE_UNIT(unit::ZibengDog);
   ADD_SELECTABLE_UNIT(unit::RageTank);
   ADD_SELECTABLE_UNIT(unit::SmokeBombTank);
-
+  ADD_SELECTABLE_UNIT(unit::phantom_of_cosin)
   unit.reset();
 }
 }  // namespace battle_game
---
+++ src/battle_game/core/units/phantom_of_cosin.cpp
@@ -0,0 +1,44 @@
+#include "battle_game/core/units/phantom_of_cosin.h"
+
+#include "battle_game/core/bullets/bullets.h"
+#include "battle_game/core/game_core.h"
+#include "battle_game/graphics/graphics.h"
+
+namespace battle_game::unit {
+
+phantom_of_cosin::phantom_of_cosin(GameCore *game_core,
+                                   uint32_t id,
+                                   uint32_t player_id)
+    : Tank(game_core, id, player_id) {
+}
+
+void phantom_of_cosin::Render() {
+  Tank::Render();
+}
+
+void phantom_of_cosin::Update() {
+  TankMove(3.0f, glm::radians(180.0f));
+  TurretRotate();
+  Fire();
+}
+
+void phantom_of_cosin::Fire() {
+  Tank::Fire();
+}
+
+bool phantom_of_cosin::IsHit(glm::vec2 position) const {
+  if (isbeenhit == 2) {
+    return Tank::IsHit(position);
+  }
+  isbeenhit = 2;
+    return false;
+}
+
+const char *phantom_of_cosin::UnitName() const {
+  return "phantom_of_cosin";
+}
+
+const char *phantom_of_cosin::Author() const {
+  return "csccsru";
+}
+}  // namespace battle_game::unit
