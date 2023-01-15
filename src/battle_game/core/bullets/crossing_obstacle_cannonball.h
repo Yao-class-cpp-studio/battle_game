37302522650f7265e5b551698642fd9e6e4c88f6
beyondwzk666 <113498606+beyondwzk666@users.noreply.github.com>
2023-01-15 23:17:48 +0800

    Update crossing_obstacle_cannonball.h

--- src/battle_game/core/bullets/crossing_obstacle_cannonball.h
+++ src/battle_game/core/bullets/crossing_obstacle_cannonball.h
@@ -2,17 +2,17 @@
 #include "battle_game/core/bullet.h"
 
 namespace battle_game::bullet {
-class crossing_obstacles_cannonball : public Bullet {
+class CrossingObstaclesCannonball : public Bullet {
  public:
-  crossing_obstacles_cannonball(GameCore *core,
-                                uint32_t id,
-                                uint32_t unit_id,
-                                uint32_t player_id,
-                                glm::vec2 position,
-                                float rotation,
-                                float damage_scale,
-                                glm::vec2 velocity);
-  ~crossing_obstacles_cannonball() override;
+  CrossingObstaclesCannonball(GameCore *core,
+                              uint32_t id,
+                              uint32_t unit_id,
+                              uint32_t player_id,
+                              glm::vec2 position,
+                              float rotation,
+                              float damage_scale,
+                              glm::vec2 velocity);
+  ~CrossingObstaclesCannonball() override;
   void Render() override;
   void Update() override;
 
