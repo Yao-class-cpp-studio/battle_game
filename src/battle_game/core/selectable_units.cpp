8f74fea9d0b8e16e279b8a5873a0bf086bdd1373
csccsru <113533448+csccsru@users.noreply.github.com>
2023-01-15 15:34:24 +0800

    debug

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -44,6 +44,7 @@ void GameCore::GeneratePrimaryUnitList() {
   ADD_SELECTABLE_UNIT(unit::ZibengDog);
   ADD_SELECTABLE_UNIT(unit::RageTank);
   ADD_SELECTABLE_UNIT(unit::SmokeBombTank);
+
   ADD_SELECTABLE_UNIT(unit::phantom_of_cosin);
   unit.reset();
 }
