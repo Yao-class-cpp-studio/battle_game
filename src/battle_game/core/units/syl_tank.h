9657a5e703090a3db1459ef0bd192741d3670f86
Yule Su <109954678+su040202@users.noreply.github.com>
2023-01-15 17:55:16 +0800

    task 2 of syl

--- src/battle_game/core/units/syl_tank.h
+++ src/battle_game/core/units/syl_tank.h
@@ -1,2 +1,31 @@
 #pragma once
 #include "battle_game/core/unit.h"
+#include "battle_game/core/units/tiny_tank.h"
+
+namespace battle_game::unit {
+class Syl_tank : public Tank {
+ public:
+  Syl_tank(GameCore *game_core, uint32_t id, uint32_t player_id);
+  void Render() override;
+  void Update() override;
+  [[nodiscard]] bool IsHit(glm::vec2 position) const override;
+
+ protected:
+  void Fire();
+  void ThePowerFromAbyss();
+  void ThePowerFromAbyssClick();
+  void TheSalvationFromStars();
+  void TheBlessOfGod();
+  void TheSalvationFromStarsClick();
+  [[nodiscard]] float GetDamageScale() const override;
+  [[nodiscard]] const char *UnitName() const override;
+  [[nodiscard]] const char *Author() const override;
+
+  // float turret_rotation_{0.0f};
+  uint32_t fire_count_down_{0};
+  uint32_t ThePowerFromAbyss_count_down_{600};
+  uint32_t TheSalvationFromStars_count_down_{900};
+  uint32_t isBurning{0};
+  bool isBlessed{false};
+};
+}  // namespace battle_game::unit
