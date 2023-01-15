f9b15791391586409a20495f95c42d2dec0547bd
liyitang22 <113581917+liyitang22@users.noreply.github.com>
2023-01-15 11:38:59 +0800

    format

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "battle_game/core/units/Whaooooo_tank.h"
+
 #include "battle_game/core/units/crit_tank.h"
 #include "battle_game/core/units/dark_fury.h"
 #include "battle_game/core/units/double_scatter_tank.h"
@@ -15,11 +15,12 @@
 #include "battle_game/core/units/sparky.h"
 #include "battle_game/core/units/spell_caster.h"
 #include "battle_game/core/units/square_tank.h"
+#include "battle_game/core/units/tangtang_tank.h"
 #include "battle_game/core/units/tankK.h"
 #include "battle_game/core/units/tank_xxy.h"
 #include "battle_game/core/units/three_body_man.h"
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/triple_shot_tank.h"
 #include "battle_game/core/units/udongein.h"
+#include "battle_game/core/units/Whaooooo_tank.h"
 #include "battle_game/core/units/zibeng_dog.h"
-#include "battle_game/core/units/tangtang_tank.h"
