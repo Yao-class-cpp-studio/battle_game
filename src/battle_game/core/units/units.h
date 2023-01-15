f6a71e86816eb5d3eeab33fac9b8212f45a8cbb9
Kaixin_Zhang <113507528+kx-zhang22@users.noreply.github.com>
2023-01-15 11:30:22 +0800

    Adding regeneration and a new moving system to collision tank in #379

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -3,3 +3,4 @@
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/laser_tank.h"
 #include "battle_game/core/units/machine_tank.h"
+#include "battle_game/core/units/collision_tank.h"
