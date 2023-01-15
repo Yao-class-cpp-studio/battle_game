4e3cdecf315091067dd5c14ad28b7d3a2f75fee9
jason huang <haofenghuang03@gmail.com>
2023-01-15 17:18:55 +0800

    customized tank version 2.0

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -1,10 +1,9 @@
 #pragma once
-
-#include "battle_game/core/units/Whaooooo_tank.h"
 #include "battle_game/core/units/crit_tank.h"
 #include "battle_game/core/units/dark_fury.h"
 #include "battle_game/core/units/double_scatter_tank.h"
 #include "battle_game/core/units/inferno_tank.h"
+#include "battle_game/core/units/lightening.h"
 #include "battle_game/core/units/lm_tank.h"
 #include "battle_game/core/units/mine_sample_tank.h"
 #include "battle_game/core/units/missile_tank.h"
@@ -21,9 +20,5 @@
 #include "battle_game/core/units/three_body_man.h"
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/triple_shot_tank.h"
-
-#include "battle_game/core/units/lightening.h"
-
 #include "battle_game/core/units/udongein.h"
 #include "battle_game/core/units/zibeng_dog.h"
-
