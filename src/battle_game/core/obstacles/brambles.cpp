2d6a03dd9293bc49583055580c6bb6254be88fa1
RYQ-22 <113417175+RYQ-22@users.noreply.github.com>
2023-01-15 17:02:39 +0800

    Update brambles.cpp

--- src/battle_game/core/obstacles/brambles.cpp
+++ src/battle_game/core/obstacles/brambles.cpp
@@ -2,7 +2,7 @@
 #include "battle_game/core/game_core.h"
 namespace battle_game::obstacle {
 
-Brambles::Brambles(GameCore *game_core, uint32_t id,glm::vec2 position,float rotation,glm::vec2 scale): Obstacle(game_core, id, position, rotation) {
+Brambles::Brambles(GameCore *game_core, uint32_t id, glm::vec2 position, float rotation, glm::vec2 scale): Obstacle(game_core, id, position, rotation) {
 }
 
 bool Brambles::IsBlocked(glm::vec2 p) const {
