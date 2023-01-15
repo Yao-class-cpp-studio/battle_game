31f149cfc9ce2443a9776a660b83afc4634f57b0
ProductiveProfessionalProgrammer <wang-yi22@mails.tsinghua.edu.cn>
2023-01-15 18:45:46 +0800

    Retry commit

--- src/battle_game/core/particles/particles.h
+++ src/battle_game/core/particles/particles.h
@@ -1,7 +1,7 @@
 #pragma once
 #include "battle_game/core/particles/bullet_hole.h"
 #include "battle_game/core/particles/charge.h"
-#include "battle_game/core/particles/heal.h"
 #include "battle_game/core/particles/explosion.h"
+#include "battle_game/core/particles/heal.h"
 #include "battle_game/core/particles/smoke.h"
 #include "battle_game/core/particles/thunderbolt.h"
