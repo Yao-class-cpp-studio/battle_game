404edc75d709fcb4a9413e0c8cf11b57d8ac7caf
beyondwzk666 <wuzk22@mails.tsinghua.edu.cn>
2023-01-15 17:16:05 +0800

    task2 and #263

---
+++ src/battle_game/core/units/tank_wzk.cpp
@@ -0,0 +1,52 @@
+#include "tank_wzk.h"
+
+#include "battle_game/core/bullets/bullets.h"
+#include "battle_game/core/game_core.h"
+#include "battle_game/graphics/graphics.h"
+
+namespace battle_game::unit {
+
+Tankwzk::Tankwzk(GameCore *game_core, uint32_t id, uint32_t player_id)
+    : Tank(game_core, id, player_id) {
+}
+
+void Tankwzk::Render() {
+  Tank::Render();
+}
+
+void Tankwzk::Update() {
+  TankMove(3.0f, glm::radians(180.0f));
+  TurretRotate();
+  Fire();
+}
+
+bool Tankwzk::IsHit(glm::vec2 position) const {
+  return Tank::IsHit(position);
+}
+
+void Tankwzk::Fire() {
+  if (fire_count_down_) {
+    fire_count_down_--;
+  } else {
+    auto player = game_core_->GetPlayer(player_id_);
+    if (player) {
+      auto &input_data = player->GetInputData();
+      if (input_data.mouse_button_down[GLFW_MOUSE_BUTTON_LEFT]) {
+        auto velocity = Rotate(glm::vec2{0.0f, 20.0f}, turret_rotation_);
+        GenerateBullet<bullet::crossing_obstacles_cannonball>(
+            position_ + Rotate({0.0f, 1.2f}, turret_rotation_),
+            turret_rotation_, GetDamageScale(), velocity);
+        fire_count_down_ = kTickPerSecond;  // Fire interval 1 second.
+      }
+    }
+  }
+}
+
+const char *Tankwzk::UnitName() const {
+  return "Tankwzk";
+}
+
+const char *Tankwzk::Author() const {
+  return "beyondwzk666";
+}
+}  // namespace battle_game::unit
