f0509df348556fc7812864d0cb6c0e74b20cdba8
xyiiiiiii <yi-xu22@mails.tsinghua.edu.cn>
2023-01-16 00:37:46 +0800

    task2

---
+++ src/battle_game/core/units/xy_tank.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "battle_game/core/unit.h"
+
+namespace battle_game::unit {
+class XyTank : public Tank {
+ public:
+  XyTank(GameCore *game_core, uint32_t id, uint32_t player_id);
+  void Render() override;
+  void Update() override;
+  [[nodiscard]] bool IsHit(glm::vec2 position) const override;
+
+ protected:
+  void TankMove(float move_speed, float rotate_angular_speed);
+  void TurretRotate();
+  void Fire();
+  [[nodiscard]] const char *UnitName() const override;
+  [[nodiscard]] const char *Author() const override;
+
+};
+}  // namespace battle_game::unit
