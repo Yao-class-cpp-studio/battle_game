1e5d2d26372aed58b0bb15d2a27efd716a3ae94f
Yule Su <109954678+su040202@users.noreply.github.com>
2023-01-16 00:34:40 +0800

    new unit assassin

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -52,6 +52,7 @@ void GameCore::GeneratePrimaryUnitList() {
   ADD_SELECTABLE_UNIT(unit::Railgun);
   ADD_SELECTABLE_UNIT(unit::Udongein);
   ADD_SELECTABLE_UNIT(unit::Syl_tank);
+  ADD_SELECTABLE_UNIT(unit::Assassin_syl);
 
   unit.reset();
 }
