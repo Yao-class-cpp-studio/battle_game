c492f84b4a255e3ac18b106428f99560ac142c66
Xiaoyu1125 <113403600+Xiaoyu1125@users.noreply.github.com>
2023-01-15 16:00:25 +0800

    Add files via upload

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -21,13 +21,8 @@
 #include "battle_game/core/units/three_body_man.h"
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/triple_shot_tank.h"
-#include "battle_game/core/units/RLUD_tank.h"
-#include "battle_game/core/units/newturret_tank.h"
-#include "battle_game/core/units/soilder_Xiaoyu.h"
 #include "battle_game/core/units/udongein.h"
 #include "battle_game/core/units/zibeng_dog.h"
-
-
 
 
 
