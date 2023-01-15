c5ba15b2e5352cd182596c252cfc918b5942df7b
jiayu-Liu0407 <113532048+jiayu-Liu0407@users.noreply.github.com>
2023-01-15 19:43:12 +0800

    Update units.h

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "battle_game/core/units/Whaooooo_tank.h"
+#include "battle_game/core/units/collision_tank.h"
 #include "battle_game/core/units/crit_tank.h"
 #include "battle_game/core/units/dark_fury.h"
 #include "battle_game/core/units/double_scatter_tank.h"
 #include "battle_game/core/units/inferno_tank.h"
+#include "battle_game/core/units/laser_tank.h"
+#include "battle_game/core/units/ljy_tank.h"
 #include "battle_game/core/units/lm_tank.h"
 #include "battle_game/core/units/mine_sample_tank.h"
 #include "battle_game/core/units/missile_tank.h"
@@ -23,6 +26,3 @@
 #include "battle_game/core/units/triple_shot_tank.h"
 #include "battle_game/core/units/udongein.h"
 #include "battle_game/core/units/zibeng_dog.h"
-#include "battle_game/core/units/ljy_tank.h"
-#include "battle_game/core/units/laser_tank.h"
-#include "battle_game/core/units/collision_tank.h"
