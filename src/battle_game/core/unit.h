2198fd3ea16c5c0efd5f806bcfd44622400326eb
SUZ-tsinghua <113418002+SUZ-tsinghua@users.noreply.github.com>
2023-01-15 21:39:30 +0800

    Update unit.h

--- src/battle_game/core/unit.h
+++ src/battle_game/core/unit.h
@@ -96,7 +96,7 @@ class Unit : public Object {
   glm::vec4 front_lifebar_color_{};
   glm::vec4 background_lifebar_color_{};
   glm::vec4 fadeout_lifebar_color_{};
-  bool operate_mode_;
+  bool operate_mode_ = 0;
 
  private:
   float fadeout_health_;
