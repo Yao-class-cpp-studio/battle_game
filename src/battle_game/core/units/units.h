f2e45277500894b56e44433efff4c4031af3f642
sunwc22 <swc645128426@outlook.com>
2023-01-16 02:34:57 +0800

    7

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -8,6 +8,7 @@
 #include "battle_game/core/units/lm_tank.h"
 #include "battle_game/core/units/mine_sample_tank.h"
 #include "battle_game/core/units/missile_tank.h"
+#include "battle_game/core/units/ordinary_tank.h"
 #include "battle_game/core/units/rage_tank_yangyr.h"
 #include "battle_game/core/units/railgun.h"
 #include "battle_game/core/units/rebounding_sample_tank.h"
@@ -23,4 +24,4 @@
 #include "battle_game/core/units/triple_shot_tank.h"
 #include "battle_game/core/units/udongein.h"
 #include "battle_game/core/units/zibeng_dog.h"
-#include "battle_game/core/units/ordinary_tank.h"
+//
