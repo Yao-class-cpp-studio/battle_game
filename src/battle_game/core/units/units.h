d3605d6e43db4fddb4e131bb688763f2e75498ec
xyiiiiiii <yi-xu22@mails.tsinghua.edu.cn>
2023-01-16 17:35:29 +0800

    task2

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -22,4 +22,5 @@
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/triple_shot_tank.h"
 #include "battle_game/core/units/udongein.h"
+#include "battle_game/core/units/xy_tank.h"
 #include "battle_game/core/units/zibeng_dog.h"
