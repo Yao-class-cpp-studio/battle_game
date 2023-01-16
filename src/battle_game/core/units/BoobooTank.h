1332f0905211230a24251a5ae906c61b0486df65
TLLtangling <113684721+TLLtangling@users.noreply.github.com>
2023-01-16 16:23:31 +0800

    Update BoobooTank.h

--- src/battle_game/core/units/BoobooTank.h
+++ src/battle_game/core/units/BoobooTank.h
@@ -6,6 +6,7 @@ namespace battle_game::unit {
 class BoobooTank : public Tank {
  public:
   BoobooTank(GameCore *game_core, uint32_t id, uint32_t player_id);
+  void Update() override;
 
  protected:
   void Fire();
