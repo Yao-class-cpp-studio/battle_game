e527eb320f74cbf42c576edf8f2d20e7e42bcf41
cylic14790382 <liuyinch22@mails.tsinghua.edu.cn>
2023-01-16 19:53:37 +0800

    trying clang

--- src/battle_game/core/units/snowball_sample_sledge.h
+++ src/battle_game/core/units/snowball_sample_sledge.h
@@ -4,16 +4,20 @@
 
 namespace battle_game::unit {
 class Snowball_Sample_Sledge : public Tank {
+ private:
+  uint32_t id_{0};
+
  public:
   Snowball_Sample_Sledge(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
   void Update() override;
+
  protected:
-  void Fire();
   void AntigravityClick();
   void Antigravity();
   void Antigravity_();
   void Antigravity_fire();
+  void Fire();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
   uint32_t fire_count_down_{0};
@@ -21,8 +25,5 @@ class Snowball_Sample_Sledge : public Tank {
   uint32_t shoot_count_down_{0};
   uint32_t hidden_count_down_{181};
   float speed_{5.0f};
-
- private:
-  uint32_t id_{0};
 };
 }  // namespace battle_game::unit
