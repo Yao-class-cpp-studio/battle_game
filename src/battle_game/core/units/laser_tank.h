45160677fed00cc7eb5fbb6f85c71183ace117c9
jiayu-Liu0407 <113532048+jiayu-Liu0407@users.noreply.github.com>
2023-01-16 10:58:25 +0800

    Update laser_tank.h

--- src/battle_game/core/units/laser_tank.h
+++ src/battle_game/core/units/laser_tank.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "battle_game/core/unit.h"
+#include "battle_game/core/units/tiny_tank.h"
 
 namespace battle_game::unit {
-class Laser_Tank : public Unit {
+class Laser_Tank : public Tank {
  public:
   Laser_Tank(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
@@ -10,19 +11,16 @@ class Laser_Tank : public Unit {
   [[nodiscard]] bool IsHit(glm::vec2 position) const override;
 
  protected:
-  void TankMove(float move_speed, float rotate_angular_speed);
-  void TurretRotate();
   void Destroy();
   void DestroyClick();
   void Fire();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
 
-  float turret_rotation_{0.0f};
   uint32_t fire_count_down_{0};
-  bool iscombo{false};
+  bool iscombo_{false};
   bool is_stuck_by_swamp{false};
   uint32_t destroy_count_down_{600};
-  uint32_t isDestroy = 0;
+  uint32_t isDestroy_ = 0;
 };
 }  // namespace battle_game::unit
