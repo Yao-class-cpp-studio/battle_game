f75a2a3174c9abe34a02985c23acacd2e6f36e07
xh092113 <lixinghan2013@163.com>
2023-01-17 00:24:59 +0800

    modified units.h & selectable_units.cpp to get compatible with type T72

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -23,3 +23,5 @@
 #include "battle_game/core/units/triple_shot_tank.h"
 #include "battle_game/core/units/udongein.h"
 #include "battle_game/core/units/zibeng_dog.h"
+#include "battle_game/core/units/t72.h"
+
