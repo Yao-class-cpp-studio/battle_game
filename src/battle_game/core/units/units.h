4ba9a88baf0b9cd46f31e5c2f588fe32eaeb2a69
xyiiiiiii <113424730+xyiiiiiii@users.noreply.github.com>
2023-01-16 21:56:43 +0800

    Update units.h

--- src/battle_game/core/units/units.h
+++ src/battle_game/core/units/units.h
@@ -22,5 +22,4 @@
 #include "battle_game/core/units/tiny_tank.h"
 #include "battle_game/core/units/triple_shot_tank.h"
 #include "battle_game/core/units/udongein.h"
-#include "battle_game/core/units/xy_tank.h"
 #include "battle_game/core/units/zibeng_dog.h"
