b685cccad53bd94f2618da34035454b88464265b
Kaixin_Zhang <113507528+kx-zhang22@users.noreply.github.com>
2023-01-16 11:05:14 +0800

    Conflicts solving

--- src/battle_game/core/obstacles/obstacles.h
+++ src/battle_game/core/obstacles/obstacles.h
@@ -1,6 +1,3 @@
 #pragma once
 #include "battle_game/core/obstacles/block.h"
-#include "battle_game/core/obstacles/rebounding_block.h"
-#include "battle_game/core/obstacles/river.h"
-#include "battle_game/core/obstacles/safety_declaration.h"
 #include "battle_game/core/obstacles/swamp.h"
