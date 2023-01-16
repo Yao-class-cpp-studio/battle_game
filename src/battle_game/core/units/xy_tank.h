ca4bca5747a2ef84940b1aae67c9aad48fec2e72
xyiiiiiii <113424730+xyiiiiiii@users.noreply.github.com>
2023-01-16 19:06:50 +0800

    Update xy_tank.h

--- src/battle_game/core/units/xy_tank.h
+++ src/battle_game/core/units/xy_tank.h
@@ -7,11 +7,9 @@ class XyTank : public Tank {
   XyTank(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
   void Update() override;
-  [[nodiscard]] bool IsHit(glm::vec2 position) const override;
 
  protected:
   void TankMove(float move_speed, float rotate_angular_speed);
-  void TurretRotate();
   void Fire();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
