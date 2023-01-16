617632d44f94b21bbb1b3f3793b950044d8e98ee
zhouxs22 <zhou-xs22@mails.tsinghua.edu.cn>
2023-01-16 19:20:40 +0800

    peatest

--- src/battle_game/core/units/pea_tank.h
+++ src/battle_game/core/units/pea_tank.h
@@ -4,10 +4,10 @@
 namespace battle_game::unit {
 class PeaTank : public Tank {
  public:
-  void Fire();
   PeaTank(GameCore *game_core, uint32_t id, uint32_t player_id);
 
  protected:
+  void fire();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
 };
