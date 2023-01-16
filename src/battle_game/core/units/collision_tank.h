d72996394738353608e5ba6a00fc95039dd1e4b1
jiayu-Liu0407 <113532048+jiayu-Liu0407@users.noreply.github.com>
2023-01-16 10:54:47 +0800

    Update collision_tank.h

--- src/battle_game/core/units/collision_tank.h
+++ src/battle_game/core/units/collision_tank.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "battle_game/core/unit.h"
+#include "battle_game/core/units/tiny_tank.h"
 
 namespace battle_game::unit {
-class CollisionTank : public Unit {
+class CollisionTank : public Tank {
  public:
   CollisionTank(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
@@ -11,16 +12,11 @@ class CollisionTank : public Unit {
   [[nodiscard]] bool IsHit(glm::vec2 position) const override;
 
  protected:
-  void TankMove(float move_speed, float rotate_angular_speed);
   void AlreadyHit();
-  void TurretRotate();
   void Fire();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
 
-  float turret_rotation_{0.0f};
-  uint32_t fire_count_down_{0};
-  uint32_t mine_count_down_{0};
-  std::map<uint32_t, bool> alreadyhit;
+  std::map<uint32_t, bool> alreadyhit_;
 };
 }  // namespace battle_game::unit
