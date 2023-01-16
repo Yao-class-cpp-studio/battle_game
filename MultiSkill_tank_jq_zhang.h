86d8795667887e9771f834d76943612b356cc625
jq-zhang22 <113609166+jq-zhang22@users.noreply.github.com>
2023-01-17 00:42:17 +0800

    Add files via upload

---
+++ MultiSkill_tank_jq_zhang.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "battle_game/core/unit.h"
+#include "battle_game/core/units/tiny_tank.h"
+
+namespace battle_game::unit {
+class MultiTank : public Tank {
+ public:
+  MultiTank(GameCore *game_core, uint32_t id, uint32_t player_id);
+  void Render() override;
+  void Update() override;
+
+ protected:
+  int firetimes_ = 0;
+  void MultiFire();
+  [[nodiscard]] const char *UnitName() const override;
+  [[nodiscard]] const char *Author() const override;
+};
+}  // namespace battle_game::unit
