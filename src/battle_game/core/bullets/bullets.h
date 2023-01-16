4fa533fef10f01e91332da242a13151d51b949db
XieRujian <113436713+XieRujian@users.noreply.github.com>
2023-01-16 10:24:42 +0800

    code format revision

--- src/battle_game/core/bullets/bullets.h
+++ src/battle_game/core/bullets/bullets.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "battle_game/core/bullets/boomerang.h"
 #include "battle_game/core/bullets/cannon_ball.h"
 #include "battle_game/core/bullets/coin.h"
 #include "battle_game/core/bullets/crit_bullet.h"
@@ -10,7 +11,6 @@
 #include "battle_game/core/bullets/rocket.h"
 #include "battle_game/core/bullets/smoke_bomb.h"
 #include "battle_game/core/bullets/sweaty_soybean.h"
-#include "battle_game/core/bullets/boomerang.h"
 #include "battle_game/core/bullets/udongein_directional_bullet.h"
 #include "battle_game/core/bullets/warning_line.h"
 #include "battle_game/core/bullets/water_drop.h"
