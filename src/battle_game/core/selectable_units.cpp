f990cd9e427d8051689d62701a3d50a93931305d
zcszcs22 <x-gao22@tsinghua.org.cn>
2023-01-16 15:12:27 +0800

    how to delete

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -29,6 +29,7 @@ void GameCore::GeneratePrimaryUnitList() {
   /*
    * TODO: Add Your Unit Here!
    * */
+
   ADD_SELECTABLE_UNIT(unit::Tank);
   ADD_SELECTABLE_UNIT(unit::Tank_gx);
   ADD_SELECTABLE_UNIT(unit::DoubleScatterTank);
