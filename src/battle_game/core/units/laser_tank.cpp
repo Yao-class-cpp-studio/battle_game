9bc8f0a2e676fda5ea3319a09471aa2817e803a3
jiayu-Liu0407 <113532048+jiayu-Liu0407@users.noreply.github.com>
2023-01-16 10:55:38 +0800

    Update laser_tank.cpp

--- src/battle_game/core/units/laser_tank.cpp
+++ src/battle_game/core/units/laser_tank.cpp
@@ -22,12 +22,12 @@ void Laser_Tank::Fire() {
     if (player) {
       auto &input_data = player->GetInputData();
       if (input_data.mouse_button_down[GLFW_MOUSE_BUTTON_LEFT]) {
-        if (isDestroy != 0) {
+        if (isDestroy_ != 0) {
           DestroyClick();
         } else {
           auto velocity = Rotate(glm::vec2{0.0f, 20.0f}, turret_rotation_);
-          if (!iscombo) {
-            iscombo = true;
+          if (!iscombo_) {
+            iscombo_ = true;
             for (int i = 1; i <= 6; i++) {
               GenerateBullet<bullet::Laser>(
                   glm::vec2{0.0f, 0.0f}, turret_rotation_,
@@ -37,10 +37,10 @@ void Laser_Tank::Fire() {
         }
       } else {
         fire_count_down_ = kTickPerSecond / 3;
-        if ((iscombo == true) && (isDestroy != 0)) {
-          isDestroy = 0;
+        if ((iscombo_ == true) && (isDestroy_ != 0)) {
+          isDestroy_ = 0;
         }
-        iscombo = false;
+        iscombo_ = false;
       }
     }
   }
@@ -48,8 +48,8 @@ void Laser_Tank::Fire() {
 
 void Laser_Tank::DestroyClick() {
   auto velocity = Rotate(glm::vec2{0.0f, 20.0f}, turret_rotation_);
-  if (!iscombo) {
-    iscombo = true;
+  if (!iscombo_) {
+    iscombo_ = true;
     GenerateBullet<bullet::Laser>(glm::vec2{0.0f, 0.0f}, turret_rotation_,
                                   GetDamageScale() * 4.0f, velocity, 1, 2);
   }
@@ -57,7 +57,7 @@ void Laser_Tank::DestroyClick() {
 
 void Laser_Tank::Destroy() {
   skills_[0].time_remain = destroy_count_down_;
-  if ((destroy_count_down_ != 0) && (isDestroy == 0)) {
+  if ((destroy_count_down_ != 0) && (isDestroy_ == 0)) {
     destroy_count_down_--;
   } else {
     auto player = game_core_->GetPlayer(player_id_);
@@ -65,7 +65,7 @@ void Laser_Tank::Destroy() {
       auto &input_data = player->GetInputData();
       if (input_data.key_down[GLFW_KEY_E] &&
           !input_data.key_down[GLFW_MOUSE_BUTTON_LEFT]) {
-        isDestroy = 1;
+        isDestroy_ = 1;
         destroy_count_down_ = 10 * kTickPerSecond;
       }
     }
@@ -73,7 +73,7 @@ void Laser_Tank::Destroy() {
 }
 
 Laser_Tank::Laser_Tank(GameCore *game_core, uint32_t id, uint32_t player_id)
-    : Unit(game_core, id, player_id) {
+    : Tank(game_core, id, player_id) {
   Skill temp;
   temp.name = "Destroy";
   temp.description = "Huge Damage";
@@ -82,61 +82,6 @@ Laser_Tank::Laser_Tank(GameCore *game_core, uint32_t id, uint32_t player_id)
   temp.type = E;
   temp.function = SKILL_ADD_FUNCTION(Laser_Tank::Destroy);
   skills_.push_back(temp);
-  if (!~tank_body_model_index) {
-    auto mgr = AssetsManager::GetInstance();
-    {
-      /* Tank Body */
-      tank_body_model_index = mgr->RegisterModel(
-          {
-              {{-0.8f, 0.8f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              {{-0.8f, -1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              {{0.8f, 0.8f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              {{0.8f, -1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              // distinguish front and back
-              {{0.6f, 1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              {{-0.6f, 1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-          },
-          {0, 1, 2, 1, 2, 3, 0, 2, 5, 2, 4, 5});
-    }
-
-    {
-      /* Tank Turret */
-      std::vector<ObjectVertex> turret_vertices;
-      std::vector<uint32_t> turret_indices;
-      const int precision = 60;
-      const float inv_precision = 1.0f / float(precision);
-      for (int i = 0; i < precision; i++) {
-        auto theta = (float(i) + 0.5f) * inv_precision;
-        theta *= glm::pi<float>() * 2.0f;
-        auto sin_theta = std::sin(theta);
-        auto cos_theta = std::cos(theta);
-        turret_vertices.push_back({{sin_theta * 0.5f, cos_theta * 0.5f},
-                                   {0.0f, 0.0f},
-                                   {0.7f, 0.7f, 0.7f, 1.0f}});
-        turret_indices.push_back(i);
-        turret_indices.push_back((i + 1) % precision);
-        turret_indices.push_back(precision);
-      }
-      turret_vertices.push_back(
-          {{0.0f, 0.0f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_vertices.push_back(
-          {{-0.1f, 0.0f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_vertices.push_back(
-          {{0.1f, 0.0f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_vertices.push_back(
-          {{-0.1f, 1.2f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_vertices.push_back(
-          {{0.1f, 1.2f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_indices.push_back(precision + 1 + 0);
-      turret_indices.push_back(precision + 1 + 1);
-      turret_indices.push_back(precision + 1 + 2);
-      turret_indices.push_back(precision + 1 + 1);
-      turret_indices.push_back(precision + 1 + 2);
-      turret_indices.push_back(precision + 1 + 3);
-      tank_turret_model_index =
-          mgr->RegisterModel(turret_vertices, turret_indices);
-    }
-  }
 }
 
 void Laser_Tank::Render() {
@@ -153,50 +98,6 @@ void Laser_Tank::Update() {
   TurretRotate();
   Destroy();
   Fire();
-}
-
-void Laser_Tank::TankMove(float move_speed, float rotate_angular_speed) {
-  auto player = game_core_->GetPlayer(player_id_);
-  if (player) {
-    auto &input_data = player->GetInputData();
-    glm::vec2 offset{0.0f};
-    if (input_data.key_down[GLFW_KEY_W]) {
-      offset.y += 1.0f;
-    }
-    if (input_data.key_down[GLFW_KEY_S]) {
-      offset.y -= 1.0f;
-    }
-    float speed = move_speed * GetSpeedScale();
-    offset *= kSecondPerTick * speed;
-    auto new_position =
-        position_ + glm::vec2{glm::rotate(glm::mat4{1.0f}, rotation_,
-                                          glm::vec3{0.0f, 0.0f, 1.0f}) *
-                              glm::vec4{offset, 0.0f, 0.0f}};
-    if (!game_core_->IsBlockedByObstacles(new_position)) {
-      game_core_->PushEventMoveUnit(id_, new_position);
-    }
-    float rotation_offset = 0.0f;
-    if (input_data.key_down[GLFW_KEY_A]) {
-      rotation_offset += 1.0f;
-    }
-    if (input_data.key_down[GLFW_KEY_D]) {
-      rotation_offset -= 1.0f;
-    }
-    rotation_offset *= kSecondPerTick * rotate_angular_speed * GetSpeedScale();
-    game_core_->PushEventRotateUnit(id_, rotation_ + rotation_offset);
-  }
-}
-
-void Laser_Tank::TurretRotate() {
-  auto player = game_core_->GetPlayer(player_id_);
-  if (player) {
-    auto &input_data = player->GetInputData();
-    auto diff = input_data.mouse_cursor_position - position_;
-    if (glm::length(diff) < 1e-4) {
-      turret_rotation_ = rotation_;
-    }
-    turret_rotation_ = std::atan2(diff.y, diff.x) - glm::radians(90.0f);
-  }
 }
 
 bool Laser_Tank::IsHit(glm::vec2 position) const {
