b02e114d40930532e020db36120ce30aab67f78d
XieRujian <113436713+XieRujian@users.noreply.github.com>
2023-01-16 12:43:35 +0800

    Add files via upload

---
+++ src/battle_game/core/units/accumulator_bullet.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "battle_game/core/bullets/cannon_ball.h"
+
+namespace battle_game::bullet {
+class AccumulatorBullet : public CannonBall {
+ public:
+  AccumulatorBullet(GameCore *core,
+                    uint32_t id,
+                    uint32_t unit_id,
+                    uint32_t player_id,
+                    glm::vec2 position,
+                    float rotation,
+                    float damage_scale,
+                    glm::vec2 velocity);
+
+  void Render() override;
+
+ private:
+  glm::vec2 velocity_{};
+};
+}  // namespace battle_game::bullet
