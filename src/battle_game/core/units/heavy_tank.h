fd8b4fcd47a9cc06a4739078414eb147e22f3f99
skydogli <48849836+skydogli@users.noreply.github.com>
2023-01-16 08:50:03 +0800

    Add files via upload

---
+++ src/battle_game/core/units/heavy_tank.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "battle_game/core/unit.h"
+#include "battle_game/core/units/tiny_tank.h"
+
+namespace battle_game::unit {
+class HeavyTank : public Tank {
+ public:
+  HeavyTank(GameCore *game_core, uint32_t id, uint32_t player_id);
+  void Render() override;
+  void Update() override;
+
+ protected:
+  void Fire();
+ // void Scatter();
+  [[nodiscard]] const char *UnitName() const override;
+  [[nodiscard]] const char *Author() const override;
+
+  // float turret_rotation_{0.0f};
+  uint32_t fire_count_down_{0};
+  uint32_t second_scatter_count_down_{0};
+  bool is_second_scatter_{false};
+};
+}  // namespace battle_game::unit
