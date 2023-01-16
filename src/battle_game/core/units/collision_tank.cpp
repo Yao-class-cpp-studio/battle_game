6fd5d5cdf3884eda317b208b0b1ed40aa70af8fa
jiayu-Liu0407 <113532048+jiayu-Liu0407@users.noreply.github.com>
2023-01-16 10:54:18 +0800

    Update collision_tank.cpp

--- src/battle_game/core/units/collision_tank.cpp
+++ src/battle_game/core/units/collision_tank.cpp
@@ -16,62 +16,7 @@ uint32_t tank_turret_model_index = 0xffffffffu;
 CollisionTank::CollisionTank(GameCore *game_core,
                              uint32_t id,
                              uint32_t player_id)
-    : Unit(game_core, id, player_id) {
-  if (!~tank_body_model_index) {
-    auto mgr = AssetsManager::GetInstance();
-    {
-      /* Tank Body */
-      tank_body_model_index = mgr->RegisterModel(
-          {
-              {{-0.8f, 0.8f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              {{-0.8f, -1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              {{0.8f, 0.8f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              {{0.8f, -1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              // distinguish front and back
-              {{0.6f, 1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-              {{-0.6f, 1.0f}, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}},
-          },
-          {0, 1, 2, 1, 2, 3, 0, 2, 5, 2, 4, 5});
-    }
-
-    {
-      /* Tank Turret */
-      std::vector<ObjectVertex> turret_vertices;
-      std::vector<uint32_t> turret_indices;
-      const int precision = 60;
-      const float inv_precision = 1.0f / float(precision);
-      for (int i = 0; i < precision; i++) {
-        auto theta = (float(i) + 0.5f) * inv_precision;
-        theta *= glm::pi<float>() * 2.0f;
-        auto sin_theta = std::sin(theta);
-        auto cos_theta = std::cos(theta);
-        turret_vertices.push_back({{sin_theta * 0.5f, cos_theta * 0.5f},
-                                   {0.0f, 0.0f},
-                                   {0.7f, 0.7f, 0.7f, 1.0f}});
-        turret_indices.push_back(i);
-        turret_indices.push_back((i + 1) % precision);
-        turret_indices.push_back(precision);
-      }
-      turret_vertices.push_back(
-          {{0.0f, 0.0f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_vertices.push_back(
-          {{-0.1f, 0.0f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_vertices.push_back(
-          {{0.1f, 0.0f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_vertices.push_back(
-          {{-0.1f, 1.2f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_vertices.push_back(
-          {{0.1f, 1.2f}, {0.0f, 0.0f}, {0.7f, 0.7f, 0.7f, 1.0f}});
-      turret_indices.push_back(precision + 1 + 0);
-      turret_indices.push_back(precision + 1 + 1);
-      turret_indices.push_back(precision + 1 + 2);
-      turret_indices.push_back(precision + 1 + 1);
-      turret_indices.push_back(precision + 1 + 2);
-      turret_indices.push_back(precision + 1 + 3);
-      tank_turret_model_index =
-          mgr->RegisterModel(turret_vertices, turret_indices);
-    }
-  }
+    : Tank(game_core, id, player_id) {
 }
 
 void CollisionTank::Render() {
@@ -89,7 +34,7 @@ void CollisionTank::InitAlreadyHit() {
   auto &units = game_core_->GetUnits();
   for (auto &unit : units) {
     if (unit.first != player_id_) {
-      alreadyhit[unit.first] = false;
+      alreadyhit_[unit.first] = false;
     }
   }
 }
@@ -101,61 +46,16 @@ void CollisionTank::Update() {
   Fire();
 }
 
-void CollisionTank::TurretRotate() {
-  auto player = game_core_->GetPlayer(player_id_);
-  if (player) {
-    auto &input_data = player->GetInputData();
-    auto diff = input_data.mouse_cursor_position - position_;
-    if (glm::length(diff) < 1e-4) {
-      turret_rotation_ = rotation_;
-    } else {
-      turret_rotation_ = std::atan2(diff.y, diff.x) - glm::radians(90.0f);
-    }
-  }
-}
-
-void CollisionTank::TankMove(float move_speed, float rotate_angular_speed) {
-  auto player = game_core_->GetPlayer(player_id_);
-  if (player) {
-    auto &input_data = player->GetInputData();
-    glm::vec2 offset{0.0f};
-    if (input_data.key_down[GLFW_KEY_W]) {
-      offset.y += 1.0f;
-    }
-    if (input_data.key_down[GLFW_KEY_S]) {
-      offset.y -= 1.0f;
-    }
-    float speed = move_speed * GetSpeedScale();
-    offset *= kSecondPerTick * speed;
-    auto new_position =
-        position_ + glm::vec2{glm::rotate(glm::mat4{1.0f}, rotation_,
-                                          glm::vec3{0.0f, 0.0f, 1.0f}) *
-                              glm::vec4{offset, 0.0f, 0.0f}};
-    if (!game_core_->IsBlockedByObstacles(new_position)) {
-      game_core_->PushEventMoveUnit(id_, new_position);
-    }
-    float rotation_offset = 0.0f;
-    if (input_data.key_down[GLFW_KEY_A]) {
-      rotation_offset += 1.0f;
-    }
-    if (input_data.key_down[GLFW_KEY_D]) {
-      rotation_offset -= 1.0f;
-    }
-    rotation_offset *= kSecondPerTick * rotate_angular_speed * GetSpeedScale();
-    game_core_->PushEventRotateUnit(id_, rotation_ + rotation_offset);
-  }
-}
-
 void CollisionTank::AlreadyHit() {
   auto player = game_core_->GetPlayer(player_id_);
   auto &input_data = player->GetInputData();
   auto &units = game_core_->GetUnits();
   for (auto &unit : units) {
     if (unit.first != player_id_) {
-      alreadyhit[unit.first] = ((alreadyhit[unit.first] == true) &&
-                                (unit.second->IsHit(position_) == true))
-                                   ? true
-                                   : false;
+      alreadyhit_[unit.first] = ((alreadyhit_[unit.first] == true) &&
+                                 (unit.second->IsHit(position_) == true))
+                                    ? true
+                                    : false;
     }
   }
 }
@@ -173,10 +73,10 @@ void CollisionTank::Fire() {
           continue;
         }
         if ((unit.first != id_) && (unit.second->IsHit(position_)) &&
-            (alreadyhit[unit.first] == false)) {
+            (alreadyhit_[unit.first] == false)) {
           game_core_->PushEventDealDamage(unit.first, id_, 20.0f);
           game_core_->PushEventDealDamage(id_, unit.first, 5.0f);
-          alreadyhit[unit.first] = true;
+          alreadyhit_[unit.first] = true;
         }
       }
       fire_count_down_ = kTickPerSecond;  // Fire interval 1 second.
