53a24247eae789f27ce62041100e35fd5c685661
ninemilewalk <2698441386@qq.com>
2023-01-17 13:13:54 +0800

    .

--- src/battle_game/core/units/random_tank.h
+++ src/battle_game/core/units/random_tank.h
@@ -4,11 +4,12 @@
 
 namespace battle_game::unit {
 class RandomTank : public Tank {
- public:
   RandomTank(GameCore *game_core, uint32_t id, uint32_t player_id);
   void RandomFire();
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
+
+ protected:
   uint32_t random_number_{2};
 };
 }  // namespace battle_game::unit
