c9338ecacdb02edc52908d2a64458b05c1255386
jq-zhang22 <113609166+jq-zhang22@users.noreply.github.com>
2023-01-18 22:05:31 +0800

    Update and rename MultiSkill_tank_jq_zhang.h to MultiSkill_tank.h

--- src/battle_game/core/units/MultiSkill_tank.h
+++ src/battle_game/core/units/MultiSkill_tank.h
@@ -8,10 +8,14 @@ class MultiTank : public Tank {
   MultiTank(GameCore *game_core, uint32_t id, uint32_t player_id);
   void Render() override;
   void Update() override;
+  [[nodiscard]] bool IsHit(glm::vec2 position) const override;
 
  protected:
-  int firetimes_ = 0;
+  int firetime_ = 0;
   void MultiFire();
+  void Shield();
+  bool be_protected = false;
+  uint32_t protect_time_ = 60;
   [[nodiscard]] const char *UnitName() const override;
   [[nodiscard]] const char *Author() const override;
 };
