a736862ccd21f9a55b2de62f4a3cfbac72e93813
jq-zhang22 <113609166+jq-zhang22@users.noreply.github.com>
2023-01-18 22:04:07 +0800

    Update MultiSkill_tank_jq_zhang.cpp

--- src/battle_game/core/units/MultiSkill_tank_jq_zhang.cpp
+++ src/battle_game/core/units/MultiSkill_tank_jq_zhang.cpp
@@ -1,4 +1,4 @@
-#include "MultiSkill_tank_jq_zhang.h"
+#include "MultiSkill_tank.h"
 
 #include "battle_game/core/bullets/bullets.h"
 #include "battle_game/core/game_core.h"
@@ -17,6 +17,7 @@ void MultiTank::Update() {
   TankMove(10.0f, glm::radians(180.0f));
   TurretRotate();
   MultiFire();
+  Shield();
 }
 
 void MultiTank::MultiFire() {
@@ -31,18 +32,21 @@ void MultiTank::MultiFire() {
         GenerateBullet<bullet::CannonBall>(
             position_ + Rotate({0.0f, 1.2f}, turret_rotation_),
             turret_rotation_, GetDamageScale(), velocity);
-        firetimes_ += 1;
-        if (firetimes_ % 15 == 5) {
+        firetime_ += 1;
+        if (firetime_ % 20 == 5) {
             GenerateBullet<bullet::ReboundingBall>(
                 position_ + Rotate({0.0f, 1.2f}, turret_rotation_),
                 turret_rotation_, GetDamageScale(), velocity);
         }
-        if (firetimes_ % 15 == 10) {
+        if (firetime_ % 20 == 10) {
             GenerateBullet<bullet::Rocket>(
                 position_ + Rotate({0.0f, 1.2f}, turret_rotation_),
                 turret_rotation_, GetDamageScale(), velocity);
         }
-        if (firetimes_ % 15 == 0) {
+        if (firetime_ % 20 == 15) {
+            Shield();
+        }
+        if (firetime_ % 20 == 0) {
             GenerateBullet<bullet::WaterDrop>(
                 position_ + Rotate({0.0f, 1.2f}, turret_rotation_),
                 turret_rotation_, GetDamageScale(), velocity);
@@ -50,13 +54,38 @@ void MultiTank::MultiFire() {
         if (IsHit(position_))
           fire_count_down_ = kTickPerSecond;
         else
-          fire_count_down_ = kTickPerSecond * 2;  // Fire interval 1 second.
+          fire_count_down_ = kTickPerSecond * 2;  // Fire interval 2 second.
       }
     }
   }
 }
 
-const char *MultiTank::UnitName() const {
+void MultiTank::Shield() {
+  skills_[0].time_remain = protect_time_;
+  if (protect_time_) {
+    protect_time_--;
+  } else if (be_protected == false) {
+    auto player = game_core_->GetPlayer(player_id_);
+    if (player) {
+      auto &input_data = player->GetInputData();
+      if (input_data.key_down[GLFW_KEY_E]) {
+        protect_time_ = 10 * kTickPerSecond;
+        be_protected = true;
+      }
+    }
+  }
+}
+
+bool MultiTank::IsHit(glm::vec2 position) const {
+  if (be_protected == true)
+    return false; 
+  position = WorldToLocal(position);
+  return position.x > -0.8f && position.x < 0.8f && position.y > -1.0f &&
+         position.y < 1.0f && position.x + position.y < 1.6f &&
+         position.y - position.x < 1.6f;
+}
+
+  const char *MultiTank::UnitName() const {
   return "Multibullet Tank";
 }
 
